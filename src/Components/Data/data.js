164f81225852e10405afc06ecf41d12da1932aa8
JOS-RE <gopidi.joshith@gmail.com>
2022-03-29 00:42:37 +0530

    Added Data JSON

---
+++ src/Components/Data/data.js
@@ -0,0 +1,28 @@
+const data1=[
+    {
+     Crop : " Wheat" ,
+     spaceTaken  :  "35 tonnes" ,
+     spaceLeft  :  "500 space" ,
+     MSP  :  "2200 / tonne" ,
+     Pricing  :  "1000" ,
+    },
+    {
+         Crop  :  "Barley" ,
+         spaceTaken  :  "55 tonnes" ,
+         spaceLeft  :  "500 space ",
+         MSP  :  "1800 / tonne ",
+         Pricing  :  "1000" ,
+    },
+    {
+         Crop  :  "Daal" ,
+         spaceTaken  : " 25 tonnes" ,
+         spaceLeft  :  "500 space ",
+         MSP  :  "1500 / tonne" ,
+         Pricing  :  "1000" ,
+    },
+
+
+]
+
+export default data1
+   
